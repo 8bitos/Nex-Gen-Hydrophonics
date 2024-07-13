5ab7ab41a99931dfba2908655e119a67db858e30
8bitos <saptaw2801@gmail.com>
2024-07-14 02:31:48 +0800

    new release

---
+++ lib/tes_setup.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tim1/firebase_options.dart';
+
+Future<void> setupFirebaseAuthMocks() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
