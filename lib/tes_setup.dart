a4a266f9c2b56e8a6c231321f070116a44bcdc06
8bitos <saptaw2801@gmail.com>
2024-07-14 02:45:48 +0800

    new release

--- lib/tes_setup.dart
+++ lib/tes_setup.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tim1/firebase_options.dart';
